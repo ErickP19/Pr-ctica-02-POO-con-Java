c4b2ce64a2c8a8a2d4797328adb45d92b569c0b8
erics <erics@172.16.212.95>
2019-04-13 01:20:05 -0500

    Practica02 Herencias

--- src/ec/ups/edu/clases/EntrenadorPersonal.java
+++ src/ec/ups/edu/clases/EntrenadorPersonal.java
@@ -5,10 +5,72 @@
  */
 package ec.ups.edu.clases;
 
+import java.util.Date;
+
 /**
  *
  * @author erics
  */
-public class EntrenadorPersonal {
-    
+public final class EntrenadorPersonal extends Entrenador {
+
+        private Entrenador entrenador;
+        private String titulo;
+        private String tipoIdioma;
+        private String rutina;
+        private Date fechadeContratacion;
+        private Date vencimientoContrato;
+
+    public Entrenador getEntrenador() {
+        return entrenador;
+    }
+
+    public void setEntrenador(Entrenador entrenador) {
+        this.entrenador = entrenador;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getTipoIdioma() {
+        return tipoIdioma;
+    }
+
+    public void setTipoIdioma(String tipoIdioma) {
+        this.tipoIdioma = tipoIdioma;
+    }
+
+    public String getRutina() {
+        return rutina;
+    }
+
+    public void setRutina(String rutina) {
+        this.rutina = rutina;
+    }
+
+    public Date getFechadeContratacion() {
+        return fechadeContratacion;
+    }
+
+    public void setFechadeContratacion(Date fechadeContratacion) {
+        this.fechadeContratacion = fechadeContratacion;
+    }
+
+    public Date getVencimientoContrato() {
+        return vencimientoContrato;
+    }
+
+    public void setVencimientoContrato(Date vencimientoContrato) {
+        this.vencimientoContrato = vencimientoContrato;
+    }
+
+    @Override
+    public String toString() {
+        return "EntrenadorPersonal{" + "entrenador=" + entrenador + ", titulo=" + titulo + ", tipoIdioma=" + tipoIdioma + ", rutina=" + rutina + ", fechadeContratacion=" + fechadeContratacion + ", vencimientoContrato=" + vencimientoContrato + '}';
+    }
+                
 }
